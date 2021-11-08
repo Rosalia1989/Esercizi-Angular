ff6c5922916b177cf376021a9f82ad9fb8a31c2b
Rosalia Spatoliatore <rosalia.spatoliatore@gmail.com>
2021-11-08 20:14:30 +0100

    exercise-1 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserContainerComponent } from './user/user-container/user-container.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { OrderContainerComponent } from './order/order-container/order-container.component';
+import { OrderListComponent } from './order/order-list/order-list.component';
+import { OrderDetailComponent } from './order/order-detail/order-detail.component';
+import { OrderSingleComponent } from './order/order-single/order-single.component';
+import { UserSingleComponent } from './user/user-single/user-single.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserContainerComponent,
+    UserListComponent,
+    UserDetailComponent,
+    OrderContainerComponent,
+    OrderListComponent,
+    OrderDetailComponent,
+    OrderSingleComponent,
+    UserSingleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
